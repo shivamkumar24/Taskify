2aa0fb92360055ca361dfb933219c1570eb50211
Shivam <shivam2432000@gmail.com>
2023-07-21 13:17:42 +0530

    This is half work complete

--- typescript-todo/src/App.tsx
+++ typescript-todo/src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./App.css";
+import InputField from "./Components/InputField";
+import TodoList from "./Components/TodoList";
+import { Todo } from "./Models/model";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Taskify</h1>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
-}
+};
 
 export default App;
